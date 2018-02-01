e96dbc56bbaa4f0fbd9fb7dd4faf9b3871ddb123
mimiya0578 <36024187+mimiya0578@users.noreply.github.com>
2018-02-01 19:04:50 +0800

    Add files via upload

---
+++ VAMod/app/src/main/java/io/virtualapp/VApp.java
@@ -0,0 +1,164 @@
+package io.virtualapp;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Looper;
+import android.support.multidex.MultiDexApplication;
+import android.util.Log;
+
+import com.flurry.android.FlurryAgent;
+import com.lody.virtual.client.core.CrashHandler;
+import com.lody.virtual.client.core.InstallStrategy;
+import com.lody.virtual.client.core.VirtualCore;
+import com.lody.virtual.client.stub.VASettings;
+import com.lody.virtual.helper.utils.DeviceUtil;
+import com.lody.virtual.helper.utils.FileUtils;
+import com.lody.virtual.helper.utils.VLog;
+import com.lody.virtual.os.VEnvironment;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import io.virtualapp.delegate.MyAppRequestListener;
+import io.virtualapp.delegate.MyComponentDelegate;
+import io.virtualapp.delegate.MyPhoneInfoDelegate;
+import io.virtualapp.delegate.MyTaskDescriptionDelegate;
+import io.virtualapp.update.VAVersionService;
+import jonathanfinerty.once.Once;
+import me.weishu.exposed.LogcatService;
+
+/**
+ * @author Lody
+ */
+public class VApp extends MultiDexApplication {
+
+    private static final String TAG = "VApp";
+
+    public static final String XPOSED_INSTALLER_PACKAGE = "de.robv.android.xposed.installer";
+
+    private static VApp gApp;
+    private SharedPreferences mPreferences;
+
+    public static VApp getApp() {
+        return gApp;
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        mPreferences = base.getSharedPreferences("va", Context.MODE_MULTI_PROCESS);
+        VASettings.ENABLE_IO_REDIRECT = true;
+        VASettings.ENABLE_INNER_SHORTCUT = false;
+        try {
+            VirtualCore.get().startup(base);
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onCreate() {
+        gApp = this;
+        super.onCreate();
+        VirtualCore virtualCore = VirtualCore.get();
+        virtualCore.initialize(new VirtualCore.VirtualInitializer() {
+
+            @Override
+            public void onMainProcess() {
+                Once.initialise(VApp.this);
+                new FlurryAgent.Builder()
+                        .withLogEnabled(true)
+                        .withListener(() -> {
+                            // nothing
+                        })
+                        .build(VApp.this, "TWJ6CT7F55SH7VFWNRZ2");
+
+                boolean isXposedInstalled = VirtualCore.get().isAppInstalled(XPOSED_INSTALLER_PACKAGE);
+                try {
+                    File oldXposedInstallerApk = getFileStreamPath("XposedInstaller_1_13.apk");
+                    if (oldXposedInstallerApk.exists()) {
+                        VirtualCore.get().uninstallPackage(XPOSED_INSTALLER_PACKAGE);
+                        oldXposedInstallerApk.delete();
+                        isXposedInstalled = false;
+                        Log.d(TAG, "remove xposed installer success!");
+                    }
+                } catch (Throwable e) {
+                    VLog.d(TAG, "remove xposed install failed.", e);
+                }
+
+                if (!isXposedInstalled) {
+                    File xposedInstallerApk = getFileStreamPath("XposedInstaller_1_24.apk");
+                    if (!xposedInstallerApk.exists()) {
+                        InputStream input = null;
+                        OutputStream output = null;
+                        try {
+                            input = getApplicationContext().getAssets().open("XposedInstaller_3.1.5.apk_");
+                            output = new FileOutputStream(xposedInstallerApk);
+                            byte[] buffer = new byte[1024];
+                            int length;
+                            while ((length = input.read(buffer)) > 0) {
+                                output.write(buffer, 0, length);
+                            }
+                        } catch (Throwable e) {
+                            VLog.e(TAG, "copy file error", e);
+                        } finally {
+                            FileUtils.closeQuietly(input);
+                            FileUtils.closeQuietly(output);
+                        }
+                    }
+
+                    if (xposedInstallerApk.isFile() && !DeviceUtil.isMeizuBelowN()) {
+                        VirtualCore.get().installPackage(xposedInstallerApk.getPath(), InstallStrategy.TERMINATE_IF_EXIST);
+                    }
+                }
+
+                // check for update
+                new android.os.Handler().postDelayed(() ->
+                        VAVersionService.checkUpdate(VApp.this), 10000);
+            }
+
+            @Override
+            public void onVirtualProcess() {
+                //listener components
+                virtualCore.setComponentDelegate(new MyComponentDelegate());
+                //fake phone imei,macAddress,BluetoothAddress
+                virtualCore.setPhoneInfoDelegate(new MyPhoneInfoDelegate());
+                //fake task description's icon and title
+                virtualCore.setTaskDescriptionDelegate(new MyTaskDescriptionDelegate());
+                virtualCore.setCrashHandler(new CrashHandler() {
+                    @Override
+                    public void handleUncaughtException(Thread t, Throwable e) {
+                        Log.i(TAG, "uncaught :" + t, e);
+                        if (t == Looper.getMainLooper().getThread()) {
+                            System.exit(0);
+                        } else {
+                            Log.e(TAG, "ignore uncaught exception of thread: " + t);
+                        }
+                    }
+                });
+                // ensure the logcat service alive when every virtual process start.
+                LogcatService.start(VApp.this, VEnvironment.getDataUserPackageDirectory(0, XPOSED_INSTALLER_PACKAGE));
+            }
+
+            @Override
+            public void onServerProcess() {
+                virtualCore.setAppRequestListener(new MyAppRequestListener(VApp.this));
+                virtualCore.addVisibleOutsidePackage("com.tencent.mobileqq");
+                virtualCore.addVisibleOutsidePackage("com.tencent.mobileqqi");
+                virtualCore.addVisibleOutsidePackage("com.tencent.minihd.qq");
+                virtualCore.addVisibleOutsidePackage("com.tencent.qqlite");
+                virtualCore.addVisibleOutsidePackage("com.facebook.katana");
+                virtualCore.addVisibleOutsidePackage("com.whatsapp");
+                virtualCore.addVisibleOutsidePackage("com.tencent.mm");
+                virtualCore.addVisibleOutsidePackage("com.immomo.momo");
+            }
+        });
+    }
+
+    public static SharedPreferences getPreferences() {
+        return getApp().mPreferences;
+    }
+
+}
---
+++ VAMod/app/src/main/java/io/virtualapp/about/AboutActivity.java
@@ -0,0 +1,111 @@
+package io.virtualapp.about;
+
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.Calendar;
+
+import io.virtualapp.R;
+import io.virtualapp.abs.ui.VActivity;
+import mehdi.sakout.aboutpage.AboutPage;
+import mehdi.sakout.aboutpage.Element;
+import moe.feng.alipay.zerosdk.AlipayZeroSdk;
+
+/**
+ * author: weishu on 18/1/12.
+ */
+public class AboutActivity extends VActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        View aboutPage = new AboutPage(this)
+                .isRTL(false)
+                .setImage(R.mipmap.ic_launcher)
+                .addItem(getVersionElement())
+                .addItem(getFeedbackElement())
+                .addEmail("twsxtd@gmail.com")
+                .addWebsite("https://github.com/android-hacker/VAExposed")
+                .addGitHub("tiann")
+                .addItem(getDonateElement())
+                .addItem(getCopyRightsElement())
+                .create();
+
+        setContentView(aboutPage);
+    }
+
+    Element getCopyRightsElement() {
+        Element copyRightsElement = new Element();
+        final String copyrights = String.format(getString(R.string.copy_right), Calendar.getInstance().get(Calendar.YEAR));
+        copyRightsElement.setTitle(copyrights);
+        copyRightsElement.setIconDrawable(R.drawable.about_icon_copy_right);
+        copyRightsElement.setIconTint(mehdi.sakout.aboutpage.R.color.about_item_icon_color);
+        copyRightsElement.setIconNightTint(android.R.color.white);
+        copyRightsElement.setGravity(Gravity.CENTER);
+        copyRightsElement.setOnClickListener(v ->
+                Toast.makeText(v.getContext(), copyrights, Toast.LENGTH_SHORT).show());
+        return copyRightsElement;
+    }
+
+    Element getVersionElement() {
+        Element version = new Element();
+        String versionName = "unknown";
+        try {
+            PackageInfo packageInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+            versionName = packageInfo.versionName;
+        } catch (PackageManager.NameNotFoundException ignored) {
+        }
+        version.setTitle(getResources().getString(R.string.about_version_title, versionName));
+        return version;
+    }
+
+    Element getFeedbackElement() {
+        Element feedback = new Element();
+        final String qqGroup = "597478474";
+        feedback.setTitle(getResources().getString(R.string.about_feedback_title, qqGroup));
+
+        feedback.setOnClickListener(v -> {
+            ClipboardManager clipboardManager = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+            if (clipboardManager != null) {
+                clipboardManager.setPrimaryClip(ClipData.newPlainText(null, qqGroup));
+            }
+            Toast.makeText(v.getContext(), getResources().getString(R.string.about_feedback_tips), Toast.LENGTH_SHORT).show();
+        });
+        return feedback;
+    }
+
+    Element getDonateElement() {
+        Element donate = new Element();
+        donate.setTitle(getResources().getString(R.string.about_donate_title));
+        donate.setIconDrawable(R.drawable.ic_menu_donate);
+        donate.setOnClickListener(v -> {
+            AlertDialog alertDialog = new AlertDialog.Builder(this, R.style.Theme_AppCompat_DayNight_Dialog_Alert)
+                    .setTitle(R.string.donate_dialog_title)
+                    .setMessage(R.string.donate_dialog_content)
+                    .setPositiveButton(R.string.donate_dialog_yes, (dialog, which) -> {
+                        if (!AlipayZeroSdk.hasInstalledAlipayClient(v.getContext())) {
+                            Toast.makeText(v.getContext(), R.string.prompt_alipay_not_found, Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                        AlipayZeroSdk.startAlipayClient(AboutActivity.this, "FKX016770URBZGZSR37U37");
+                    })
+                    .setNegativeButton(R.string.donate_dialog_no, null)
+                    .create();
+            try {
+                alertDialog.show();
+            } catch (Throwable ignored) {
+                // BadTokenException.
+            }
+        });
+        return donate;
+    }
+}
